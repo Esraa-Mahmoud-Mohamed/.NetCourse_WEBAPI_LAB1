6e88f1853575953847668ed350c3d7eb98c0c956
Abdo <Abdo@DESKTOP-6H9B8GJ>
2024-12-06 21:12:29 +0200

    Add project files.

---
+++ WEBAPI_LAB1/Controllers/CoursesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WEBAPI_LAB1.Models;
+
+namespace WEBAPI_LAB1.Controllers
+{
+    [Route("api/courses")]
+    [ApiController]
+    public class CoursesController : ControllerBase
+    {
+        static List<Course> courses = new List<Course>() { 
+            new Course() {ID = 1, Name = ".Net", Duration = 64},
+            new Course() {ID = 2, Name = "HTML", Duration = 96},
+            new Course() {ID = 3, Name = "C#", Duration = 144},
+        };
+
+        [HttpGet]
+        public List<Course> GetAll()
+        {
+            return courses;
+        }
+
+        [HttpGet("{name}")]
+        public Course GetByName(string name)
+        {
+            return courses.Find(c => c.Name == name);
+        }
+
+        [HttpPost]
+        public void AddCourse(Course course)
+        {
+            courses.Add(course);
+        }
+    }
+}
---
+++ WEBAPI_LAB1/Models/Course.cs
@@ -0,0 +1,9 @@
+﻿namespace WEBAPI_LAB1.Models
+{
+    public class Course
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Duration { get; set; }
+    }
+}
